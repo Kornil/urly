210a99d3e678000ccbdf20b44801a163d279151c
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-11 10:22:19 +0200

    Add simple form to interact with api

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => <div>hello</div>
+class App extends Component {
+  state = {
+    value: "",
+  };
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { value } = this.state;
+
+    const response = await fetch("/v1/links", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({ url: value })
+    });
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <main className="container">
+        <div>
+          <form onSubmit={this.handleSubmit} method="post" action="/v1/links">
+            <input onChange={this.handleChange} type="text" value={value} />
+          </form>
+        </div>
+      </main>
+    );
+  }
+}
 
 export default App;
