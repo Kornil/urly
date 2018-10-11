4445ab996823923af9caed82737c7d5cf972feee
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-11 10:21:08 +0200

    Prepare backend routes
    Using node cache as storage

--- src/server/index.js
+++ src/server/index.js
@@ -2,18 +2,50 @@ import React from "react";
 import express from "express";
 import { renderToString } from "react-dom/server";
 import { renderStylesToString } from "emotion-server";
+import bodyParser from "body-parser";
+import validUrl from "valid-url";
+import shortid from "shortid";
+import NodeCache from "node-cache";
 
 import App from "../client/App";
 import htmlMarkup from "./htmlMarkup";
 
 const app = express();
 
+const urlCache = new NodeCache();
 
 // We're going to serve up the public
 // folder since that's where our
 // client bundle.js file will end up.
 app.use(express.static("public"));
 
+app.use(bodyParser.json());
+
+app.post("/v1/links", (req, res) => {
+    const { url } = req.body;
+
+    if (validUrl.isHttpUri(url)) {
+      const shortId = shortid.generate();
+
+      urlCache.set(shortId, { url });
+
+      res.json(JSON.stringify({ hash: shortId }));
+    } else {
+      res.status(500).json(JSON.stringify("Url provided is invalid"));
+    }
+  })
+
+app.get("/v1/:hash", (req, res) => {
+  const { hash } = req.params;
+
+  urlCache.get(hash, (err, value) => {
+    if (err || value === undefined) {
+      res.status(404).json(JSON.stringify("Shortlink provided does not exist"));
+    } else {
+      res.redirect(value.url);
+    }
+  });
+});
 
 app.get("*", (req, res) => {
   // eslint-disable-next-line react/jsx-filename-extension
