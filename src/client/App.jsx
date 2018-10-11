910629dfcf46c2f8c66995d9f6d03e6cbab090b2
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-11 10:23:13 +0200

    Add FE logic to fetch all shortlinks

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,6 +3,20 @@ import React, { Component } from "react";
 class App extends Component {
   state = {
     value: "",
+    cachedUrls: {}
+  };
+
+  componentDidMount() {
+    this.fetchShortLinks();
+  }
+
+  fetchShortLinks = async () => {
+    const response = await fetch("/v1/links");
+
+    const data = await response.json();
+    this.setState({
+      cachedUrls: JSON.parse(data)
+    });
   };
 
   handleChange = event => {
@@ -25,16 +39,27 @@ class App extends Component {
     const data = await response.json();
 
     console.log(data);
+
+    this.fetchShortLinks();
   };
 
   render() {
-    const { value } = this.state;
+    const { value, cachedUrls } = this.state;
     return (
       <main className="container">
         <div>
           <form onSubmit={this.handleSubmit} method="post" action="/v1/links">
             <input onChange={this.handleChange} type="text" value={value} />
           </form>
+          {Object.keys(cachedUrls).map(key => (
+            <a
+              href={cachedUrls[key].url}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {key} - {cachedUrls[key].url}
+            </a>
+          ))}
         </div>
       </main>
     );
