588ab0a32dae8464e4a3bdf81f2e35e3f69c5822
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-10-12 10:22:45 +0200

    Switch server to 8888

--- src/server/index.js
+++ src/server/index.js
@@ -64,8 +64,8 @@ app.get("*", (req, res) => {
   res.send(htmlMarkup(markup));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port: 3000`);
+  console.log(`Server is listening on port: 8888`);
 });
